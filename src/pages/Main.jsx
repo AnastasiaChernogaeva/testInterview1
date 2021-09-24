bacc9261067670018b6ea7562f12f25701de55bd
AnastasiaChernogaeva <sergeyvhernogaev1979@gmail.com>
2021-09-24 18:11:27 +0300

    PlotlyData

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,55 +1,15 @@
 import React, {useCallback, useEffect, useState} from "react";
-import Button from 'react-bootstrap/Button';
-import {TableFilterWithTable} from '../components/TableFilter.jsx';
+import { PlotlyData } from "../components/PlotData.jsx";
 
-let serverUsers = []
 export  const Main = () => {
-    const [isClicked, setIsClicked] = useState(false)
-   
-    const handleChange =  useCallback(() =>{
-        setIsClicked(prev => !prev)
-    }, [setIsClicked] )
 
-    
-    const [users, setUsers] = useState([])
-    // const [value, setValue] = useState('')
-
-
-    useEffect(()=>{
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response => response.json())
-        .then(users => {
-            serverUsers=users
-            setUsers(users)
-        }
-            )
-    }, [])
-
-    const handleChangeInput = (e) => {
-        if(e.target.value){
-        let listOfUsers = serverUsers.slice().filter(user=>user.name.toLowerCase().includes(e.target.value.toLowerCase()))
-        setUsers(listOfUsers)
-        setValue(e.target.value)
-        }
-        else{
-            setUsers(serverUsers)
-            setValue(e.target.value)
-
-        }
         
 
-    }
+    
+    
     return(
         <div >
-            {
-                isClicked? (
-                    <TableFilterWithTable onChange={handleChangeInput} users={users} value={value}/>
-                ) : null
-
-            }
-            <Button onClick={handleChange}>
-                New World
-            </Button>
+           <PlotlyData/>
         </div>
     )
 }
